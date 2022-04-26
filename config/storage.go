68f38576f79c64a5c0576dd96a2b96c984e28840
Josh Nies <josh.nies@hey.com>
2022-04-26 11:24:24 -0400

    check that storj bucket exists on init

--- config/storage.go
+++ config/storage.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"time"
 
 	"storj.io/uplink"
 )
@@ -15,15 +17,41 @@ type StorageInstance struct {
 var SI StorageInstance
 
 func InitStorage() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Validate environment variables
+	accessGrant := os.Getenv("STORJ_ACCESS_GRANT")
+	if accessGrant == "" {
+		panic("STORJ_ACCESS_GRANT is not set")
+	}
+
+	bucket := os.Getenv("STORJ_BUCKET")
+	if bucket == "" {
+		panic("STORJ_BUCKET is not set")
+	}
+
 	// Create access grant to Storj bucket
-	access, err := uplink.ParseAccess(os.Getenv("STORJ_ACCESS_GRANT"))
+	access, err := uplink.ParseAccess(accessGrant)
 	if err != nil {
 		panic(fmt.Sprintf("Failed to authenticate with Storj: %s", err))
+	}
+
+	// Open Storj project and ensure bucket exists
+	project, err := uplink.OpenProject(ctx, access)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to open Storj project: %s", err))
+	}
+	defer project.Close()
+
+	_, err = project.EnsureBucket(ctx, bucket)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to ensure bucket %s (it probably doesn't exist): %s", bucket, err))
 	}
 
 	// Create global storage instance
 	SI = StorageInstance{
 		Access: access,
-		Bucket: os.Getenv("STORJ_BUCKET"),
+		Bucket: bucket,
 	}
 }
