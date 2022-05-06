56d77702755561c33663d05cc70973a987575a40
Josh Nies <josh.nies@hey.com>
2022-05-06 19:39:37 -0400

    add storj access to project doc

--- models/project.go
+++ models/project.go
@@ -3,8 +3,10 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Project struct {
-	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	CreatedAt int64              `json:"created_at" bson:"created_at"`
-	Name      string             `json:"name" bson:"name" validate:"required"`
+	ID                    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	CreatedAt             int64              `json:"created_at" bson:"created_at"`
+	Name                  string             `json:"name" bson:"name" validate:"required"`
+	AccessGrant           string             `json:"access_grant,omitempty" bson:"access_grant,omitempty"`
+	AccessGrantExpiration int64              `json:"access_grant_expiration,omitempty" bson:"access_grant_expiration,omitempty"`
 	// TODO: Add owner_id
 }
