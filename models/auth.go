fdd2e1d58b6ff4c2cb26d8bfc0ad852aaab400bc
Josh Nies <josh.nies@hey.com>
2022-07-05 11:03:18 -0400

    oauth

--- models/auth.go
+++ models/auth.go
@@ -3,6 +3,7 @@ package models
 // Request body for `/authenticate`
 type AuthenticateRequest struct {
 	Token        string `json:"token" validate:"required"`
+	TokenType    string `json:"token_type" validate:"required"`
 	SessionToken string `json:"session_token"`
 }
 
