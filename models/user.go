6d81805eaf5a69c89c5fb71e4725626263c43373
Josh Nies <josh.nies@hey.com>
2022-07-12 10:00:04 -0400

    add team billing models

--- models/user.go
+++ models/user.go
@@ -7,7 +7,8 @@ type UserData struct {
 	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	CreatedAt int64              `json:"created_at,omitempty" bson:"created_at,omitempty"`
 	UserID    string             `json:"user_id" bson:"user_id"`
-	Roles     []RoleObject       `json:"roles" bson:"roles"`
+	// Roles for teams and projects.
+	Roles []RoleObject `json:"roles" bson:"roles"`
 	// ID of the team that new projects will be created in by default.
 	DefaultTeamID primitive.ObjectID `json:"default_team_id" bson:"default_team_id"`
 }
