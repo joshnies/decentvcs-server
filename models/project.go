cf6f6ff8912c7e03da4754c69b5082e49d67d628
Josh Nies <josh.nies@hey.com>
2022-08-07 21:44:04 -0400

    add backdrop URL to project model

--- models/project.go
+++ models/project.go
@@ -11,14 +11,18 @@ type Project struct {
 	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
 	// Project name that must be unique in the scope of the team.
 	Name string `json:"name" bson:"name" validate:"required"`
-	// Team that owns the project.
+	// ID of the team that owns the project.
 	TeamID          primitive.ObjectID `json:"team_id,omitempty" bson:"team_id,omitempty"`
 	DefaultBranchID primitive.ObjectID `json:"default_branch_id,omitempty" bson:"default_branch_id,omitempty"`
+	// URL of the backdrop image.
+	BackdropURL string `json:"backdrop_url,omitempty" bson:"backdrop_url,omitempty"`
 }
 
 type UpdateProjectRequest struct {
 	Name            string `json:"name"`
 	DefaultBranchID string `json:"default_branch_id"`
+	// URL of the backdrop image.
+	BackdropURL string `json:"backdrop_url,omitempty" bson:"backdrop_url,omitempty"`
 }
 
 type InviteManyUsersDTO struct {
