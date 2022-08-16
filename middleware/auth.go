43b23baaee3d36e075a555242a64633c75ee80ce
Josh Nies <josh.nies@hey.com>
2022-08-16 09:13:58 -0400

    simplify middleware dir

--- middleware/auth.go
+++ middleware/auth.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/joshnies/decent-vcs/config"
 	"github.com/joshnies/decent-vcs/constants"
+	"github.com/joshnies/decent-vcs/lib/acl"
 	"github.com/joshnies/decent-vcs/lib/auth"
 	"github.com/joshnies/decent-vcs/models"
 	"github.com/stytchauth/stytch-go/v5/stytch"
@@ -38,6 +39,35 @@ func IsAuthenticated(c *fiber.Ctx) error {
 	c.SetUserContext(userCtx)
 
 	return c.Next()
+}
+
+// Fiber middleware that ensures the user has access to the requested team.
+// If `minRole` is nil, any role is allowed.
+func HasTeamAccess(minRole models.Role) func(*fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		userData := auth.GetUserDataFromContext(c)
+		teamName := c.Params("team_name")
+
+		// Check if user has access to team
+		res, err := acl.HasTeamAccess(userData, teamName, minRole)
+		if err != nil {
+			fmt.Printf("[middleware.HasTeamAccess] Failed to determine team access: %v\n", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Internal server error",
+			})
+		}
+		if !res.HasAccess {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "Unauthorized",
+			})
+		}
+
+		// Add team to user context for later use
+		ctx := context.WithValue(c.UserContext(), models.ContextKeyTeam, res.Team)
+		c.SetUserContext(ctx)
+
+		return c.Next()
+	}
 }
 
 // Middleware that adds user data to context.
