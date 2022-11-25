1f44ae9ebdd1e608dc063c0e792152c849c66eaa
Josh Nies <josh.nies@hey.com>
2022-11-25 10:17:44 -0500

    add billing routes

---
+++ routes/billing.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/decentvcs/server/controllers"
+	"github.com/decentvcs/server/middleware"
+	"github.com/gofiber/fiber/v2"
+)
+
+func RouteBilling(router fiber.Router) {
+	router.Use(middleware.IsAuthenticated)
+
+	router.Post("/subscriptions", controllers.CreateSubscription)
+}
