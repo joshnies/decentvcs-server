b4a44463543382ad045fc08b95362fa7206f7da6
Josh Nies <josh.nies@hey.com>
2022-12-26 15:10:45 -0500

    rm omitempty from project fields

--- models/project.go
+++ models/project.go
@@ -7,18 +7,18 @@ import (
 )
 
 type Project struct {
-	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	ID        primitive.ObjectID `json:"_id" bson:"_id"`
 	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
 	// Project name that must be unique in the scope of the team.
 	Name string `json:"name" bson:"name" validate:"required"`
 	// ID of the team that owns the project.
-	TeamID          primitive.ObjectID `json:"team_id,omitempty" bson:"team_id,omitempty"`
-	DefaultBranchID primitive.ObjectID `json:"default_branch_id,omitempty" bson:"default_branch_id,omitempty"`
+	TeamID          primitive.ObjectID `json:"team_id" bson:"team_id"`
+	DefaultBranchID primitive.ObjectID `json:"default_branch_id" bson:"default_branch_id"`
 	// URL of the thumbnail image.
 	ThumbnailURL string `json:"thumbnail_url,omitempty" bson:"thumbnail_url,omitempty"`
 	// If `true`, modified committed files in this project will be uploaded as patches instead of snapshots (e.g. the
 	// whole file).
-	EnablePatchRevisions bool `json:"enable_patch_revisions,omitempty" bson:"enable_patch_revisions,omitempty"`
+	EnablePatchRevisions bool `json:"enable_patch_revisions" bson:"enable_patch_revisions"`
 }
 
 type CreateProjectRequest struct {
